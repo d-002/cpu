ba4598161222c08da9cd50e35b5c7edb24fabe6e
d-002 <minecraftenjoy@outlook.fr>
2025-12-11 11:27:48 +0100

    nonfunctional import-export interface

---
+++ pussi-8/emulator/import-export.js
@@ -0,0 +1,39 @@
+import specs from "./specs.js";
+
+let state, ui;
+
+function rom2file() {
+    const exported = [];
+    state.rom.data.forEach(data => exported.push(data.getSilent()));
+    console.log(JSON.stringify(exported));
+}
+
+function file2rom() {
+    let ok = true;
+
+    let data;
+    try {
+        data = JSON.parse("");
+    }
+    catch (_) {
+        ok = false;
+    }
+
+    if (!ok || data == null || data.length != specs.romLength) {
+        console.log("Failed to import");
+        return;
+    }
+
+    for (let i = 0; i < specs.romLength; i++)
+        state.rom.data[i].setSilent(parseInt(data[i]) || 0);
+
+    ui.display();
+}
+
+export function setupImportExport(_state, _ui) {
+    state = _state;
+    ui = _ui;
+
+    document.getElementById("import").addEventListener("click", file2rom);
+    document.getElementById("export").addEventListener("click", rom2file);
+}
--- pussi-8/emulator/main.js
+++ pussi-8/emulator/main.js
@@ -1,10 +1,12 @@
 import State from "./state.js";
 import UiManager from "./ui.js";
+import { setupImportExport } from "./import-export.js";
 import { setupUserChanges } from "./user.js";
 
 const state = new State();
 const ui = new UiManager(state);
 
 setupUserChanges(state, ui);
+setupImportExport(state, ui);
 
 ui.display();
