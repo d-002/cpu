dca62571390557e18d93a6e0cb61051ac07c8d9e
d-002 <minecraftenjoy@outlook.fr>
2026-02-28 23:20:40 +0100

    assembler: added binary export

--- pussi-8/assembler/src/export/binary.c
+++ pussi-8/assembler/src/export/binary.c
@@ -10,19 +10,40 @@
 int to_binary_file(struct cli_args *args, char *path, struct queue *content)
 {
     char *export_path = change_extension(path, "bin");
+    FILE *f = NULL;
+    int err = SUCCESS;
+
     if (export_path == NULL)
         return ALLOC_ERROR;
 
     verbose(args, NO_LINE, "Exporting binary encoded program to '%s'.",
             export_path);
 
-    FILE *f = fopen(export_path, "w");
+    f = fopen(export_path, "w");
     if (f == NULL)
     {
-        logerror(NO_LINE, "Failed to write to file.");
-        return ALLOC_ERROR;
+        logerror(NO_LINE, "Failed to open file.");
+        err = IO_ERROR;
+        goto end;
     }
 
+    while (content->length)
+    {
+        short *instruction = queue_dequeue(content);
+        char buf[2] = { *instruction >> 8, *instruction & 255 };
+        int wres = fwrite(buf, sizeof(char), 2, f);
+        free(instruction);
+
+        if (wres)
+        {
+            logerror(NO_LINE, "Failed to write to file.");
+            err = IO_ERROR;
+            goto end;
+        }
+    }
+
+end:
+    fclose(f);
     free(export_path);
-    return SUCCESS;
+    return err;
 }
