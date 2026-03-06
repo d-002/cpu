e5ea52a80f87aba758d9eb0fde45de6f2c96898a
d-002 <minecraftenjoy@outlook.fr>
2026-03-06 16:16:56 +0100

    schematic: correct structure for rom

--- pussi-8/assembler/src/export/schematic.c
+++ pussi-8/assembler/src/export/schematic.c
@@ -6,6 +6,7 @@
 #include "logger/logger.h"
 #include "nbt/nbt.h"
 #include "utils/errors.h"
+#include "utils/specs.h"
 
 #define STR_LEN(s) s, strlen(s)
 
@@ -68,12 +69,12 @@ int to_schematic(struct cli_args *args, char *path, struct queue *content)
 
     // TODO: rest of metadata
 
-    // nbt_tag_t *width = nbt_new_tag_short(4 * 8);
-    // nbt_tag_t *height = nbt_new_tag_short(1 * 16);
-    // nbt_tag_t *length = nbt_new_tag_short(4 * 16);
-    nbt_tag_t *width = nbt_new_tag_short(4);
-    nbt_tag_t *height = nbt_new_tag_short(1);
-    nbt_tag_t *length = nbt_new_tag_short(4);
+    short w = 4 * 16 - 3;
+    short h = 16;
+    short l = 4 * 8;
+    nbt_tag_t *width = nbt_new_tag_short(w);
+    nbt_tag_t *height = nbt_new_tag_short(h);
+    nbt_tag_t *length = nbt_new_tag_short(l);
     nbt_set_tag_name(width, STR_LEN("Width"));
     nbt_set_tag_name(height, STR_LEN("Height"));
     nbt_set_tag_name(length, STR_LEN("Length"));
@@ -94,27 +95,58 @@ int to_schematic(struct cli_args *args, char *path, struct queue *content)
     nbt_set_tag_name(palette, STR_LEN("Palette"));
     nbt_tag_compound_append(blocks, palette);
 
-    nbt_tag_t *temp_palette_elt = nbt_new_tag_int(1);
-    nbt_set_tag_name(temp_palette_elt, STR_LEN("minecraft:rail"));
-    nbt_tag_compound_append(palette, temp_palette_elt);
+    nbt_tag_t *palette_air = nbt_new_tag_int(0);
+    nbt_set_tag_name(palette_air, STR_LEN("minecraft:air"));
+    nbt_tag_compound_append(palette, palette_air);
+    nbt_tag_t *palette_stone = nbt_new_tag_int(1);
+    nbt_set_tag_name(palette_stone, STR_LEN("minecraft:stone"));
+    nbt_tag_compound_append(palette, palette_stone);
 
-    int8_t *arr_temp = malloc(64 * sizeof(int8_t));
-    for (size_t i = 0; i < 64; i++)
-        arr_temp[i] = 1;
+    size_t data_size = (size_t)w * h * l;
+    int8_t *data_arr = calloc(data_size, sizeof(int8_t));
 
-    nbt_tag_t *blocks_data = nbt_new_tag_byte_array(arr_temp, 64);
-    nbt_set_tag_name(blocks_data, STR_LEN("Data"));
-    nbt_tag_compound_append(blocks, blocks_data);
+    // encode data in the ROM barrels pattern, meaning a 16-bits instruction is
+    // split into 4 barrels
+    if (content->length > ROM_SIZE)
+        logwarn(NO_LINE, "Program size is too large for the ROM: %ld > %ld",
+                content->length, ROM_SIZE);
+    short nop = 0;
+    for (size_t i = 0; i < ROM_SIZE; i++)
+    {
+        short *instruction = i >= content->length ? &nop
+            : i == 0                              ? queue_iter_start(content)
+                                                  : queue_iter_next(content);
+        instruction++; /////
+
+        short layer = i / 32;
+        short x = layer * 4;
+        short y1 = i % 4 * 2 + i / 4 % 2;
+        short y2 = y1 + 8;
+        short z1 = i / 4 % 8 * 4 + layer % 2;
+        short z2 = z1 + 2;
+
+        size_t i = (y1 * l + z1) * w + x;
+        data_arr[i] = 1;
+        i = (y2 * l + z1) * w + x;
+        data_arr[i] = 1;
+        i = (y1 * l + z2) * w + x;
+        data_arr[i] = 1;
+        i = (y2 * l + z2) * w + x;
+        data_arr[i] = 1;
+    }
+
+    nbt_tag_t *data = nbt_new_tag_byte_array(data_arr, data_size);
+    nbt_set_tag_name(data, STR_LEN("Data"));
+    nbt_tag_compound_append(blocks, data);
 
     nbt_tag_t *block_entities = nbt_new_tag_list(NBT_TYPE_COMPOUND);
     nbt_set_tag_name(block_entities, STR_LEN("BlockEntities"));
     nbt_tag_compound_append(blocks, block_entities);
 
     write_nbt_file(export_path, root, NBT_WRITE_FLAG_USE_GZIP);
-    free(arr_temp);
+    free(data_arr);
     nbt_free_tag(root);
 
     free(export_path);
-    content++; /////
     return SUCCESS;
 }
---
+++ pussi-8/assembler/src/utils/specs.h
@@ -0,0 +1,6 @@
+#ifndef SPECS_H
+#define SPECS_H
+
+#define ROM_SIZE 512
+
+#endif /* ! SPECS_H */
